2c88cd075b452af199b5cf8db77de330cfc504b7
Matt Leezer <mattleezer@Matts-iMac-4.lan>
2021-07-26 20:43:01 -0400

    added javascript

--- Final Project/js/scripts.js
+++ Final Project/js/scripts.js
@@ -1,40 +1,16 @@
-var Menu = {
-    el: {
-      menu: $('.menu'),
-      menuTop: $('.menu-top'),
-      menuClose: $('.menu-close'),
-      menuMiddle: $('.menu-middle'),
-      menuBottom: $('.menu-bottom'),
-      menuText: $('.menu-text')
-    },
-    
-    init: function() {
-      Menu.bindUIactions();
-    },
-    
-    bindUIactions: function() {
-      Menu.el.menu
-          .on(
-            'click',
-          function(event) {
-          Menu.activateMenu(event);
-          event.preventDefault();
-        }
-      );
-    },
-    
-    activateMenu: function() {
-      Menu.el.menuTop.toggleClass('menu-top-expand expand');
-      Menu.el.menuMiddle.toggleClass('menu-middle-expand expand');
-      Menu.el.menuBottom.toggleClass('menu-bottom-expand expand'); 
-      Menu.el.menuText.toggleClass('menu-text-expand');
-      Menu.el.menuClose.toggleClass('menu-close-visible');
-    }
-  };
-    
-    //Stop menu item click closing the menu
-    $(".menu .menu-global").click(function(e) {
-        e.stopPropagation();
-  });
-  
-  Menu.init();
+function ValidateEmail()
+{
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
